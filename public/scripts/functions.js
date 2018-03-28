db71143d3f05e6ef2005002b17ab82d9dd617d34
larakollokian <lara.kollokian@gmail.com>
2018-03-28 19:33:54 -0400

    updated function.js

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,8 +1,40 @@
 // Create Azure Account - https://azure.microsoft.com/en-us/free/free-account-students-faq/
 // Retrieve API Key - https://azure.microsoft.com/en-ca/try/cognitive-services/
 // API Documentation - https://westus.dev.cognitive.microsoft.com/docs/services/TextAnalytics.V2.0/operations/56f30ceeeda5650db055a3c9
-function submitComment(commentControl) {
-    // TODO - Call API
+function submitComment(commentControl) 
+{
+	var comments = document.getElementsByName(commentControl)[0].value;
+	var url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
+	var apiKey = "5126da30869a44d590a5e7072a2f61ad"; 
+
+	var body = 
+	{
+  		"documents": 
+  		[
+    		{
+      		"language": "en-US",
+      		"id": "1",
+      		"text": comments
+    		}
+ 		]
+	}
+
+	$.ajax({
+type: "POST",
+url: url,
+data: JSON.stringify(body),
+processData: false,
+headers: {
+"Ocp-Apim-Subscription-Key": apiKey,
+"Content-Type": "application/json"
+}
+}).done(function(result){
+var sentimentRating = Math.round((result.documents[0].score * 100)/ 25) + 1;
+var currentItemId = JSON.parse(localStorage.getItem('currentItemId'));
+window.location.href = '/comments?classId=' + currentItemId + '&rating=' + sentimentRating + '&comments=' + comments;
+}).fail(function(xhr, status, err) {
+alert(err);
+});
 }
 
 function navigateToComments(classId) {
